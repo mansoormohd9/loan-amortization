54ad9eecb3091da547648ebd030cdc11ba7e7e92
GREENWICH\smohammad <shaikmansoor.mohammad@factset.com>
2020-01-29 20:35:48 +0530

    Setting up required components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <AmortizationForm></AmortizationForm>
+    <Schedule></Schedule>
+    <Summary></Summary>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import AmortizationForm from './components/AmortizationForm.vue';
+import Schedule from './components/Schedule.vue';
+import Summary from './components/Summary.vue';
 
 @Component({
   components: {
     HelloWorld,
+    AmortizationForm,
+    Schedule,
+    Summary,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/AmortizationForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>AmortizationForm</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class AmortizationForm extends Vue {
+  
+}
+</script>
